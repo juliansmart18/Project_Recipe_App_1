9d3459c4b7fa980aa3e59b5328fd67c010797b5a
Julian <juliansmart1996@gmail.com>
2023-09-10 15:47:22 -0400

    completed styling

--- src/RecipeView.js
+++ src/RecipeView.js
@@ -7,8 +7,8 @@ function RecipeView({recipe, deleteRecipe}) {
             <td>{recipe.name}</td>
             <td>{recipe.cuisine}</td>
             <td><img src={recipe.photo} alt={recipe.name} /></td>
-            <td>{recipe.ingredients}</td>
-            <td>{recipe.preparation}</td>
+            <td className="content_td"><p>{recipe.ingredients}</p></td>
+            <td className="content_td"><p>{recipe.preparation}</p></td>
             <td><button name="delete" onClick={()=>deleteRecipe(recipe)}>Delete</button></td>
 </tr>
   );
